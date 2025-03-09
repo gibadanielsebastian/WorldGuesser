da3a10aeff110ccbb85ea3de280f8569dd3b57e1
Daniel Giba <gibysnack@gmail.com>
2025-03-09 23:28:38 +0200

    Small fix

--- src/app/gamemodes/guess-the-flag/page.js
+++ src/app/gamemodes/guess-the-flag/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { countries } from "country-flag-icons";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect, useMemo, useCallback } from "react";
